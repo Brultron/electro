b15cd93a863b94699c0db5b1f62dad4706f14884
Joseph Spear <jspear1984@gmail.com>
2016-07-18 19:14:29 -0400

    search should infinite scroll #3

--- source/components/crate/Crate.react.jsx
+++ source/components/crate/Crate.react.jsx
@@ -52,6 +52,8 @@ class Crate extends React.Component {
     }
   }
 
+  //TODO figure out what the importance of this number is... its about 2 pixels
+  // off each time on my 13" screen i think
   loadSearch(e){
     if($('.crate-inner').scrollTop() > this.refs.crateInner.scrollHeight - 757) {
       console.log('load more!')
