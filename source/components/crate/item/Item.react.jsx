fbb405df3674791ead1b149aa22abf3838aaa1c1
Joseph Spear <jspear1984@gmail.com>
2016-07-26 08:55:20 -0400

    everything is broken

--- source/components/crate/item/Item.react.jsx
+++ source/components/crate/item/Item.react.jsx
@@ -18,16 +18,26 @@ class Item extends React.Component {
   }
 
   render(){
+
+    var background_image = `
+      -webkit-linear-gradient(left, rgba(255,38,0,1) 0%, rgba(252,40,5,1) 12%, rgba(246,45,15,0) 38%, rgba(233,54,35,1) 90%, rgba(231,56,39,1) 100%)
+      url(${this.props.track.thumbnail});
+    `;
+
+    var aStyle = {
+      'background-image': background_image
+    };
+
     return (
-      <div className='crate-item'>
-        <img src={this.props.track.thumbnail}>
-        </img>
+      <div className='crate-item '>
         <div className='item-title'>
           {this.props.track.title}
         </div>
-        <a onClick={this.loadTrack}>
-          <i className="fa fa-cloud-download fa-3x lv2_blur"></i>
-        </a>
+          <a onClick={this.loadTrack}
+            className='track-thumb'
+            style={aStyle}>
+            <i className="fa fa-cloud-download fa-3x lv2_blur"></i>
+          </a>
       </div>
     );
   }
